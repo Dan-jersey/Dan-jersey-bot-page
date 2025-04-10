14ebe1af276ff1ae0edebcabc6a269e9012f7f7b
Dan-jersey <stromjersey@gmail.com>
2025-04-10 22:11:32 +0100

    Create spotify.js

---
+++ commands/spotify.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+  name: 'spotify',
+  description: 'recherche les musiques que tu veux à l'aide de leur titre.',
+  usage: 'spotify [nom de la musique]',
+  author: 'coffee',
+
+  async execute(senderId, args, pageAccessToken) {
+    try {
+      const { data } = await axios.get(`https://hiroshi-api.onrender.com/tiktok/spotify?search=${encodeURIComponent(args.join(' '))}`);
+      const link = data[0]?.download;
+
+      sendMessage(senderId, link ? {
+        attachment: { type: 'audio', payload: { url: link, is_reusable: true } }
+      } : { text: 'Sorry, no Spotify link found for that query.' }, pageAccessToken);
+    } catch {
+      sendMessage(senderId, { text: 'Sorry, there was an error processing your request.' }, pageAccessToken);
+    }
+  }
+};
